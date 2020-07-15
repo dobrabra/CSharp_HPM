f42640f1bbde348fb8a56a3b45401e21444432d9
dobrabra <60182464+dobrabra@users.noreply.github.com>
2020-07-15 16:02:47 +0200

    Add files via upload

---
+++ Interface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPM
+{
+
+    interface IWriter
+    {
+        void Write(char ch);
+    }
+
+    class Textfield : IWriter
+    {
+        public void Write(char ch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    struct Buffer : IWriter
+    {
+        //char[]buf = new char[128];
+
+        public void Write(char ch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Interface
+    {
+        static void Main(string[] args)
+        {
+            IWriter w = new Textfield();
+            w.Write('x');
+            w = new Buffer();
+            w.Write('y');
+        }
+    }
+}
